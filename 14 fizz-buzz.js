4e95fa7aa96bf4a392c333204c692588897c72f6
Nimlu <snesterov@gmail.com>
2020-09-06 16:56:39 +0300

    feat: solve 14 test

---
+++ 14 fizz-buzz.js
@@ -0,0 +1,11 @@
+function fizzbuzz(n) {
+	let arr = []
+	for (let i = 1; i <= n; i++) {
+		if (!(i % 3 || i % 5)) arr[i - 1] = "FizzBuzz";
+		else if (!(i % 3)) arr[i - 1] = "Fizz";
+		else if (!(i % 5)) arr[i - 1] = "Buzz";
+		else arr[i - 1] = i;
+	}
+	return arr;
+}
+console.log(fizzbuzz(30))
